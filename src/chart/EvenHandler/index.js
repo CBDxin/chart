87eccef9593a147acbc67bd27e54c2032bb5ca35
duxin <1102003453@qq.com>
2020-05-14 21:15:11 +0800

    [modify]修复鼠标坐标不准确问题

--- src/chart/EvenHandler/index.js
+++ src/chart/EvenHandler/index.js
@@ -2,48 +2,53 @@ export let getMouseInfo = (event, container) => {
 	if (!container) {
 		return null;
 	}
-  let containerOffset = getOffset(container);
-  let chartCoordinate = calculateChartCoordinate(event, containerOffset);
+	let containerOffset = getOffset(container);
+	let chartCoordinate = calculateChartCoordinate(event, containerOffset);
 
-  return chartCoordinate;
+	return chartCoordinate;
 };
+
+function getOffsetTop(el) {
+	return el.offsetParent ? el.offsetTop + getOffsetTop(el.offsetParent) : el.offsetTop;
+}
+
+function getOffsetLeft(el) {
+	return el.offsetParent ? el.offsetLeft + getOffsetLeft(el.offsetParent) : el.offsetLeft;
+}
 
 let getOffset = el => {
 	if (el) {
 		return {
-			left: el.current.offsetLeft,
-			top: el.current.offsetTop,
+			left: getOffsetLeft(el.current),
+			top: getOffsetTop(el.current),
 		};
 	}
 	return null;
 };
 
-let calculateChartCoordinate = (event, offset) => (
-  {
-    chartX: Math.round(event.pageX - offset.left),
-    chartY: Math.round(event.pageY - offset.top),
-  }
-);
+let calculateChartCoordinate = (event, offset) => ({
+	chartX: Math.round(event.pageX - offset.left),
+	chartY: Math.round(event.pageY - offset.top),
+});
 
-export function getActiveIndex(x, list){
-  let result;
-  list.sort((a, b)=>{
-    return a - b;
-  });
-  list.some((item, index)=>{
-    if(index === 0){
-      result = index
-    }else{
-      if(Math.abs(x - item) >= Math.abs(x - list[index - 1])){
-        result = index - 1;
-        return true;
-      }else{
-        result = index;
-        return false;
-      }
-    }
-    return false;
-  })
-  return result;
+export function getActiveIndex(x, list) {
+	let result;
+	list.sort((a, b) => {
+		return a - b;
+	});
+	list.some((item, index) => {
+		if (index === 0) {
+			result = index;
+		} else {
+			if (Math.abs(x - item) >= Math.abs(x - list[index - 1])) {
+				result = index - 1;
+				return true;
+			} else {
+				result = index;
+				return false;
+			}
+		}
+		return false;
+	});
+	return result;
 }
-
